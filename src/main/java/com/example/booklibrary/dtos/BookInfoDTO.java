dc52d586597db2bccc8ff64a361bca22e11ac7b1
unknown <kuprum321@gmail.com>
2024-03-17 14:41:31 +0300

    убрал дублирование кода

--- src/main/java/com/example/booklibrary/dtos/BookInfoDTO.java
+++ src/main/java/com/example/booklibrary/dtos/BookInfoDTO.java
@@ -1,52 +1,23 @@
 package com.example.booklibrary.dtos;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @JsonSerialize(using = BookInfoDTOSerializer.class)
 public class BookInfoDTO {
 
     private String title;
     private String genre;
     private int year;
-
     private String author;
 
     public BookInfoDTO(String title, String genre, int year, String author) {
         this.title = title;
         this.genre = genre;
         this.year = year;
-        this.author = author;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
         this.author = author;
     }
 }
--- src/main/java/com/example/booklibrary/entity/BookInfo.java
+++ src/main/java/com/example/booklibrary/entity/BookInfo.java
@@ -1,59 +1,19 @@
 package com.example.booklibrary.entity;
 
-
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "bookinfo")
 public class BookInfo {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
     private String title;
-
     private String genre;
     private int year;
-
     private String author;
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
 }
